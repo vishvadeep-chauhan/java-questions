9ab6866e350787003b61d6a0f765c18f96162d91
vishvadeep-chauhan <vishvadeepsinghchauhan@gmail.com>
2022-12-12 19:11:08 +0530

    first commit

---
+++ Array.java
@@ -0,0 +1,17 @@
+public class Array
+	{
+	 	public static void main(String [] args)
+		{
+			int A[]=new int [10];
+			int B[]={1,2,3,4,5};
+			int C[];
+			C=new int[10];
+			
+			B[2]=15;
+			
+			for (int i=0; i<B.length; i++){
+				System.out.println(B[i]);
+				} 
+
+}
+}
