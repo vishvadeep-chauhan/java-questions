4be45e39bf38ff8a0a11daddbf024697e3d28f89
vishvadeep singh chauhan <vishvadeepsinghchauhan@gmail.com>
2023-02-04 23:32:15 +0530

    ab
    ab

---
+++ intro java/print_simple_star.java
@@ -0,0 +1,11 @@
+public class print_simple_star {
+    public static void main(String[] args) {
+        // System.out.println("*");
+        // System.out.println( "**");
+        // System.out.println( "***");
+        // System.out.println("****" );
+
+        System.out.println("*\n**\n***\n****\n*****");
+
+    }
+}
---
+++ intro java/sumof245.java
@@ -0,0 +1,6 @@
+public class sumof245 {
+    public static void main(String[] args) {
+        System.out.println(+2+4+5);
+    }
+    
+}
