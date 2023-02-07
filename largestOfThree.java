e7aef352eed6758c0b4e34d9f0f37224247f122e
vishvadeep singh chauhan <vishvadeepsinghchauhan@gmail.com>
2023-02-07 18:00:29 +0530

    Master

---
+++ largestOfThree.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class largestOfThree {
+    public static void main(String[] args) {
+        System.out.println("enter three number");
+
+        Scanner s=new Scanner(System.in);
+
+        int x=s.nextInt();
+        int y=s.nextInt();
+        int z=s.nextInt();
+
+        if (x > y && x> z){
+            System.out.println("x is the largest number");
+        }
+
+        else if ( y>x && y>z ){
+
+            System.out.println("y is the largest number");
+        }
+
+        else{
+
+            System.out.println("z is the largest number");
+        }
+    }
+}
