7bb524cc29df2dc9405f92a62bba96ce35e59cbb
vishvadeep singh chauhan <vishvadeepsinghchauhan@gmail.com>
2023-02-07 19:13:45 +0530

    Master

---
+++ HackerRank Questions/if_else.java
@@ -0,0 +1,13 @@
+// n=odd and odd number is weird
+
+
+
+import java.util.Scanner;
+
+public class if_else {
+
+    public static void main(String[] args) {
+        System.out.println("");
+    }
+    
+}
